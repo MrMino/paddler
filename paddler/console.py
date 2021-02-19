74e024fe984c6ad8e93aded014d88b1e756733d0
Blazej Michalik <blazej.michalik@nokia.com>
2021-02-19 02:03:29 +0100

    Add ABCs for console components

--- paddler/console.py
+++ paddler/console.py
@@ -1,3 +1,4 @@
+from abc import ABCMeta, abstractmethod
 from queue import Queue
 
 # mypy only
@@ -6,9 +7,20 @@ from typing import Callable, Any
 
 # TODO: typehints
 
+class InputField(metaclass=ABCMeta):
+    """Abstract base for an input field of a console."""
+    accept_callback: Callable[[str], Any]
 
-# TODO: abstract class
-class TUIInputField:
+
+class OutputField(metaclass=ABCMeta):
+    """Abstract base for a multiline output field of a console."""
+
+    @abstractmethod
+    def write_line(self, text: str) -> None:
+        pass
+
+
+class TUIInputField(InputField):
     __slots__ = ('accept_callback', '_text_area')
 
     def __init__(self, text_area: TextArea):
@@ -22,8 +34,7 @@ class TUIInputField:
         self.accept_callback(text)
 
 
-# TODO: abstract class
-class TUIOutputField:
+class TUIOutputField(OutputField):
     def __init__(self, text_area: TextArea):
         self._text_area = text_area
 
@@ -36,11 +47,15 @@ class TUIOutputField:
 
 
 class Console:
-    def __init__(self, input_widget, output_widget, *, echo=True):
+    def __init__(self,
+                 input_widget: InputField,
+                 output_widget: OutputField,
+                 *, echo=True):
         self._input = input_widget
         self._output = output_widget
 
-        self._input.accept_callback = self._input_handler
+        # See python/mypy/#708
+        self._input.accept_callback = self._input_handler  # type: ignore
 
         self.echo = echo
         self.commands = Queue()
